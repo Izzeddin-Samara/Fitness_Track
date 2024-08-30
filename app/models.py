af8758de319fb057b5dbb335f5cb0e0ad6a99244
Izzsamara <izzidinsamara@gmail.com>
2024-08-30 20:50:19 +0300

    Add User model with custom UserManager and registration validation

--- app/models.py
+++ app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+import re
 
-# Create your models here.
+class UserManager(models.Manager):
+    def register_validator(self, postData, user_id=None):
+        errors = {}
+        email_check = User.objects.filter(email=postData['email'])
+        if user_id:
+            email_check = email_check.exclude(id=user_id)
+        if email_check.exists():
+            errors["email"] = "Email must be unique"
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['email_error'] = "Invalid email!"
+        if len(postData['first_name']) < 2:
+            errors["first_name"] = "First name should be at least 2 characters"
+        if len(postData['last_name']) < 2:
+            errors["last_name"] = "Last name should be at least 2 characters"
+        if len(postData['password']) < 8:
+            errors["password"] = "Password should be at least 8 characters"
+        if postData['password'] != postData['password_confirmation']:
+            errors["password_confirmation"] = "Passwords do not match"
+        return errors
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=45)
+    password = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
