0fa75c0764f4af8ada963315469b1b810dd2ace9
Izzsamara <izzidinsamara@gmail.com>
2024-09-01 11:29:04 +0300

    Add login view and URL, add necessary imports

--- app/views.py
+++ app/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
+from django.contrib import messages
+import bcrypt
 
 def index(request):
     all_the_coaches = models.show_all_coaches(request)
     return render(request, 'home.html', {'all_the_coaches': all_the_coaches})
+
+def login(request):
+    if request.method == 'POST':
+        user = models.check(request)
+        if user and bcrypt.checkpw(request.POST['password'].encode(), user.password.encode()):
+            request.session['userid'] = user.id
+            return redirect('/user_dashboard')
+        else:
+            messages.error(request, "Invalid email or password", extra_tags='login')
+            return redirect('login') 
+
+    
+    return render(request, 'login.html')
