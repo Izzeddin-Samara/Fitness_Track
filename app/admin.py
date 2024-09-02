39f34ca1ff307cdbbea3f1109a5b91d55e56a4ef
Izzsamara <izzidinsamara@gmail.com>
2024-09-02 21:47:34 +0300

    Add Coach model to Django admin with customized list display and search fields

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Coach
 
-# Register your models here.
+@admin.register(Coach)
+class CoachAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'bio', 'created_at', 'updated_at')
+    search_fields = ('first_name', 'last_name', 'email')
+
