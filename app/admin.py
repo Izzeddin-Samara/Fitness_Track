bd146f91743f71518e5417bf9e4ecab748ac04be
Izzsamara <izzidinsamara@gmail.com>
2024-09-10 18:16:22 +0300

    Enhance Coach admin with custom form and password handling

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
+from django import forms  
 from .models import Coach
+import bcrypt
 
-@admin.register(Coach)
+class CoachForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput, required=True) 
+
+    class Meta:
+        model = Coach
+        fields = ('first_name', 'last_name', 'email', 'image', 'bio', 'password')  
+
 class CoachAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email', 'bio', 'created_at', 'updated_at')
-    search_fields = ('first_name', 'last_name', 'email')
+    list_display = ('first_name', 'last_name', 'email', 'image', 'created_at')
+    form = CoachForm  
 
+    def save_model(self, request, obj, form, change):
+        if form.cleaned_data['password']:
+            
+            obj.password = bcrypt.hashpw(form.cleaned_data['password'].encode(), bcrypt.gensalt()).decode()
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Coach, CoachAdmin)
